ddfe7f8b723e186cc20b1f2d6448b0eda312fb24
bluesylveon <44065730+bluesylveon@users.noreply.github.com>
2019-03-21 13:16:39 -0400

    tp formatif2

---
+++ public_html/javascript/javascript.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector("button");
+    btn.onclick= function(){
+//        var textToRemove = document.getElementById("commencer");
+//        var parent= document.body;        
+//        parent.removeChild(textToRemove);
+        
+        
+    };
+
+//window.onload=init();
